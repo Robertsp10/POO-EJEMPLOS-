46654629668497cbb11c84e93857288ce755c1a8
Benjamin Cisneros Barraza <cisnerosbarraza@gmail>
2023-10-27 03:49:09 -0600

    Se corrige código

--- Practicas/Unidad2/Java/Main.java
+++ Practicas/Unidad2/Java/Main.java
@@ -4,23 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
         Persona persona = new Persona("Rogelio", 25);
-        
-        
 
-        System.out.println("La edad de la persona es " + persona.miEdad);
-        System.out.println("La persona vive? " + persona.vive);
+        System.out.println("La edad de la persona es " + persona.miEdad());
+        System.out.println("La persona vive? " + persona.vive());
 
         persona.saludar();
-        System.out.println("La edad de la persona  es " + persona.miEdad);
-        System.out.println("La persona vive? " + persona.vive);
+        System.out.println("La edad de la persona  es " + persona.miEdad());
+        System.out.println("La persona vive? " + persona.vive());
 
         persona.incrementaredad();
-        System.out.println("La edad de la persona es " + persona.miEdad);
-        System.out.println("La persona vive? " + persona.vive);
+        System.out.println("La edad de la persona es " + persona.miEdad());
+        System.out.println("La persona vive? " + persona.vive());
 
         persona.morir();
-        System.out.println("La edad de la persona es " + persona.miEdad);
-        System.out.println("La persona vive? " + persona.vive);
+        System.out.println("La edad de la persona es " + persona.miEdad());
+        System.out.println("La persona vive? " + persona.vive());
 
-    }String=persona.minombre();System.out.println("Nombre dela persona");
+        String nombreDeLaPersona = persona.miNombre();
+
+        System.out.println(nombreDeLaPersona);
+
+    }
 }
--- Practicas/Unidad2/Java/persona.java
+++ Practicas/Unidad2/Java/persona.java
@@ -6,7 +6,6 @@ class Persona {
     private boolean vive;
 
     Persona(String nombre, int edad) {
-
         this.nombre = nombre;
         this.edad = edad;
         this.vive = true;
@@ -16,53 +15,23 @@ class Persona {
         System.out.println("Hola mi nombre es " + nombre);
     }
 
-    void incrementaredad() {
+    void incrementarEdad() {
         edad = edad + 1;
     }
 
     void morir() {
         vive = false;
-
     }
 
-    public static void main(String[] args) {
-        Persona persona = new Persona();
-
-        persona.edad = 25;
-        persona.nombre = "Rogelio";
-        persona.vive = true;
-
-        System.out.println("La edad de la persona es " + persona.edad);
-        System.out.println("La persona vive? " + persona.vive);
-
-        persona.saludar();
-        System.out.println("La edad de la persona es " + persona.edad);
-        System.out.println("La persona vive? " + persona.vive);
-
-        persona.incrementaredad();
-        System.out.println("La edad de la persona es " + persona.edad);
-        System.out.println("La persona vive? " + persona.vive);
-
-        persona.morir();
-        System.out.println("La edad de la persona es " + persona.edad);
-        System.out.println("La persona vive? " + persona.vive);
-
+    int miEdad() {
+        return this.edad;
     }
 
-    int miEdad();
-
-    {
-
-        returnthis.edad;
-    }
-
-    boolean vive();
-
-    {
+    boolean vive() {
         return this.vive;
     }
 
-    String minombre();{
-        return.this.nombre;
+    String miNombre() {
+        return this.nombre;
     }
 }
